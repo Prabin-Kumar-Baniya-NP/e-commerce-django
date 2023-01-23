2d66deca840a59917cb04cd2a95fab20115ad7b6
Prabin Kumar Baniya <pra44bin@gmail.com>
2023-01-23 12:40:27 +0545

    Category Model Added and Created Custom Category Admin

--- ecommerce/category/admin.py
+++ ecommerce/category/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from category.models import Category
 
-# Register your models here.
+
+class CustomCategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "is_active"]
+    search_fields = ["name", "description"]
+    list_filter = ["is_active"]
+
+
+admin.site.register(Category, CustomCategoryAdmin)
--- ecommerce/category/models.py
+++ ecommerce/category/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField(max_length=1024)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
