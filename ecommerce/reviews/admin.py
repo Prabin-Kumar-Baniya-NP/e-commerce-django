db0cce67410742463edf62f146696158f29a8301
Prabin Kumar Baniya <pra44bin@gmail.com>
2023-01-24 12:13:18 +0545

    Reviews added

--- ecommerce/reviews/admin.py
+++ ecommerce/reviews/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+class CustomReviewAdmin(admin.ModelAdmin):
+    fields = ["user", "product", "rating", "comment", "image", "is_approved"]
+    list_display = ["user", "product", "rating"]
+    list_filter = ["rating"]
+    search_fields = [
+        "product__name",
+        "user__first_name",
+        "user__middle_name",
+        "user__last_name",
+    ]
+    readonly_fields = [
+        "user",
+        "product",
+        "rating",
+        "comment",
+    ]
+
+
+admin.site.register(Review, CustomReviewAdmin)
--- ecommerce/reviews/models.py
+++ ecommerce/reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from product.models import Product
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="customer", verbose_name="Customer"
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="product",
+        verbose_name="Product",
+    )
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    comment = models.TextField(max_length=1024)
+    image = models.ImageField(upload_to="reviewsImage/", null=True, blank=True)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name + " | " + self.user.get_full_name()
