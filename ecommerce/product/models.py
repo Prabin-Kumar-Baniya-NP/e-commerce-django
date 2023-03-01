8e89ae06ea96d213fc6de7ac5dfb4f52f1f1afb3
Prabin Kumar Baniya <pra44bin@gmail.com>
2023-03-01 13:12:07 +0545

    Set is_default to false for product variant

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -47,7 +47,7 @@ class ProductVariant(models.Model):
     currency = models.CharField(max_length=3, default="USD", choices=(("USD", "USD"),))
     image = models.ImageField(upload_to="productImage/", null=True, blank=True)
     is_active = models.BooleanField(default=True)
-    is_default = models.BooleanField(default=True)
+    is_default = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
